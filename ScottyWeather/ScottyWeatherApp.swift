95f82262225fda79123d0197d3a9bbab708cfd3a
scottysimply <scottistriggered@gmail.com>
2025-04-28 08:31:11 -0500

    Initial Commit

---
+++ ScottyWeather/ScottyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScottyWeatherApp.swift
+//  ScottyWeather
+//
+//  Created by PATTY, SCOTT on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScottyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
