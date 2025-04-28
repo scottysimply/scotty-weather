92261f0bb1f0b7a1710dacb592e197d16745c149
scottsimply <scottistriggered@gmail.com>
2025-04-28 10:02:30 -0500

    Created empty views for structure

---
+++ ScottyWeather/CurrentWeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeatherView.swift
+//  ScottyWeather
+//
+//  Created by PATTY, SCOTT on 4/28/25.
+//
+
+import SwiftUI
+
+struct CurrentWeatherView: View {
+    @State var weatherData: WeatherData?
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentWeatherView()
+}
---
+++ ScottyWeather/PreferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesView.swift
+//  ScottyWeather
+//
+//  Created by PATTY, SCOTT on 4/28/25.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreferencesView()
+}
